ce812952a2966fc33de4205d48d09db96ee16303
ViniciusKendy17 <135672206+ViniciusKendy17@users.noreply.github.com>
2023-11-14 21:59:45 -0300

    big changes
    Changes

---
+++ manga_hub/src/main/java/manga_hub/manga_hub/MangaHubApplication.java
@@ -0,0 +1,14 @@
+package manga_hub.manga_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaHubApplication.class, args);
+	}
+
+	}
+
---
+++ manga_hub/src/test/java/manga_hub/manga_hub/MangaHubApplicationTests.java
@@ -0,0 +1,13 @@
+package manga_hub.manga_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
