78ba26a128170ab925e078fede578934a99b9520
ViniciusKendy17 <135672206+ViniciusKendy17@users.noreply.github.com>
2023-11-19 16:05:00 -0300

    Update
    Database codes

---
+++ manga_hub/Code_BD.sql
@@ -0,0 +1,16 @@
+-- Database: manga_hub
+
+-- DROP DATABASE IF EXISTS manga_hub;
+
+CREATE DATABASE manga_hub
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
