c2bd7ae31c64d7745004e180a152e856dc5044a0
ViniciusKendy17 <135672206+ViniciusKendy17@users.noreply.github.com>
2023-11-20 20:51:32 -0300

    aaa
    aaaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/main/resources/templates/**/*.html"
+    "./src/main/resources/templates/index.html",
+    "./src/main/resources/static/js/**/*.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
