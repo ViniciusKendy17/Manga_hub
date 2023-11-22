43e1c7fb9ad5aa9281124dfeed01bee77f4b0b2d
Rafael Correa <rafaelDev3590@gmail.com>
2023-11-22 15:42:52 -0300

    Ajuste na Classe de configuração de endpoint

--- manga_hub/src/main/java/manga_hub/manga_hub/Infra/SecurityConfig.java
+++ manga_hub/src/main/java/manga_hub/manga_hub/Infra/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(authorize -> authorize
             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+            .requestMatchers(HttpMethod.GET, "/view/**").permitAll()
             .anyRequest().authenticated()
         )   .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();  
