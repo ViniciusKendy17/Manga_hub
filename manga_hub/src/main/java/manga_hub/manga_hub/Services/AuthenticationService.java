1918097bf176f3ab00183704b1359c8c6e59fc51
Rafael Correa <rafaelDev3590@gmail.com>
2023-11-22 15:17:46 -0300

    Autenticação
    Adição de registro de conta com criptografia de senha e autenticação Stateless via Token.

---
+++ manga_hub/src/main/java/manga_hub/manga_hub/Services/AuthenticationService.java
@@ -0,0 +1,35 @@
+package manga_hub.manga_hub.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import manga_hub.manga_hub.DTO.UserRegDTO;
+import manga_hub.manga_hub.models.UserModel;
+import manga_hub.manga_hub.repositories.UserRepository;
+
+@Service
+public class AuthenticationService implements UserDetailsService{
+
+    @Autowired
+    UserRepository repository;
+
+    public void save(UserRegDTO userRegDTO){
+        // Converter UserDTO para UserModel
+        UserModel userModel = new UserModel();
+        userModel.setName(userRegDTO.nome());
+        userModel.setLogin(userRegDTO.login());
+        userModel.setSenha(userRegDTO.senha());
+        userModel.setCep(userRegDTO.cep());
+        userModel.setTelefone(userRegDTO.telefone());
+        userModel = repository.save(userModel);
+    }
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByLogin(email);
+    }
+    
+}
---
+++ manga_hub/src/main/java/manga_hub/manga_hub/controllers/LoginTokenDTO.java
@@ -0,0 +1,5 @@
+package manga_hub.manga_hub.controllers;
+
+public record LoginTokenDTO(String token) {
+    
+}
