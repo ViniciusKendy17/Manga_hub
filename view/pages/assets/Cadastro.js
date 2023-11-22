566ba9f28a3e017e53e4681e6aa4526991b45892
Vinicius Kendy Munhoz de Souza <135672206+ViniciusKendy17@users.noreply.github.com>
2023-11-22 14:42:27 -0300

    aaa
    aaaa

---
+++ view/pages/assets/Cadastro.js
@@ -0,0 +1,36 @@
+
+function Cadastrar() {
+fetch("https//localhost:8080/cadastro",
+{
+    Headers:{
+        "Accept":"aplication/json",
+        "Content-Type": "aplication/json"
+    },
+
+    method:"Post",
+    body: JSON.stringify({
+
+    })
+
+        
+
+    
+
+ }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
