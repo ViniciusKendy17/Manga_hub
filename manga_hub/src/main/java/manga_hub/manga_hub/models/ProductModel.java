75247e80d28d90b73e46dccaadcb2b39d0d3f52b
Rafael Correa <rafaelDev3590@gmail.com>
2023-11-24 22:26:17 -0300

    Domain changes

--- manga_hub/src/main/java/manga_hub/manga_hub/models/ProductModel.java
+++ manga_hub/src/main/java/manga_hub/manga_hub/models/ProductModel.java
@@ -1,5 +1,8 @@
 package manga_hub.manga_hub.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.annotations.ManyToAny;
 
 import jakarta.persistence.Entity;
@@ -8,9 +11,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import manga_hub.manga_hub.Enums.Genero;
+import manga_hub.manga_hub.Enums.TipoProduto;
 
 @Entity(name = "produto")
 @Data
@@ -25,10 +31,12 @@ public class ProductModel {
     private String nome; 
     private Double preco;
     private int estoque;
-    private String tipo_produto; 
+    @OneToMany
+    private List <Genero> genero = new ArrayList<>();
+    private TipoProduto tipo_produto; 
     private String isbn; 
 
     @ManyToOne
-    @JoinColumn(name = "id_vendedor") // Adicionando o nome da coluna na tabela ProductModel
+    @JoinColumn(name = "id_vendedor")
     private UserModel id_vendedor; 
 }
