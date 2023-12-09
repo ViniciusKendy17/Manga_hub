54d63ad9055e81597375dd4480f5bb7ccff63642
Rafael Correa <rafaelDev3590@gmail.com>
2023-12-09 00:19:14 -0300

    Disposição de cards por sessão

--- manga_hub/src/main/resources/static/JS/home.js
+++ manga_hub/src/main/resources/static/JS/home.js
@@ -1,15 +1,11 @@
-      
-      
-      document.addEventListener('DOMContentLoaded', function () {
-      const cardList = document.getElementById('carousel');
-      const products = [
+const products = [
         {
           id: 1,
           titulo: "One Piece",
           subtitulo: "Mangá de One Piece novo",
           imagem: "./IMGs/olhaEssaPorrakkkkkk - Copia.png",
           preco: 100.00,
-          vendedor: "Rafael Picka de Mel",
+          vendedor: "Rafael Correa",
           descricao: "After being reborn, Zhao Mingxi had no choice but to mooch off of Fu group's young master's luck to avoid death by terminal illness. Although his ears would turn so red as if they're bleeding whenever he sees her, he still keeps up an act of prideful indifference: 'Can you stop being so clingy?' The next day.",
           id_vendedor: 123
         },
@@ -85,66 +81,83 @@
         } 
       ];
     
-      // Função para criar um card
-      function createProductCard(product) {
-        const card = document.createElement('div');
-        card.classList.add('card-product');
-        card.dataset.productId = product.id; // Adiciona o ID como um atributo de dados
+     // Função para criar um card de produto
+     function createProductCard(product) {
+      const card = document.createElement('div');
+      card.classList.add('card-product');
+      card.dataset.productId = product.id;
+
+      const imgDiv = document.createElement('div');
+      imgDiv.classList.add('image');
+      const img = document.createElement('img');
+      img.src = product.imagem;
+      img.alt = 'Product Image';
+      imgDiv.appendChild(img);
     
-        const image = document.createElement('img');
-        image.src = product.imagem;
-        image.alt = '';
+      const content = document.createElement('div');
+      content.classList.add('content');
     
-        const content = document.createElement('div');
-        content.classList.add('product-content');
+      const title = document.createElement('h3');
+      title.classList.add('product-name');
+      title.textContent = product.titulo;
     
-        const title = document.createElement('h5');
-        title.classList.add('title');
-        title.textContent = product.titulo;
+      const price = document.createElement('p');
+      price.classList.add('price');
+      price.textContent = `RS$ ${product.preco ? product.preco.toFixed(2) : 'N/A'}`;
     
-        const price = document.createElement('p');
-        price.classList.add('price');
-        price.textContent = `RS$ ${product.preco ? product.preco.toFixed(2) : 'N/A'}`;
+      const vendedor = document.createElement('p');
+      vendedor.classList.add('vendedor');
+      vendedor.textContent = `Vendedor: ${product.vendedor}`;
     
-        const vendedor = document.createElement('p');
-        vendedor.classList.add('vendedor');
-        vendedor.textContent = product.vendedor;
+      const addButton = document.createElement('button');
+      addButton.classList.add('add-to-cart-button');
+      addButton.textContent = 'Adicionar ao Carrinho';
     
-        content.appendChild(title);
-        content.appendChild(price);
-        content.appendChild(vendedor);
-    
-        card.appendChild(image);
-        card.appendChild(content);
-    
-        // Adiciona um ouvinte de evento de clique ao card
-        card.addEventListener('click', function () {
-          const productId = this.dataset.productId;
-          console.log('ID do produto clicado:', productId);
-        });
-    
-        return card;
-      }
-    
-      // Adiciona os cards à lista
-      products.forEach((product) => {
-        const card = createProductCard(product);
-        cardList.appendChild(card);
+      addButton.addEventListener('click', function () {
+        const productId = card.dataset.productId;
+        console.log('Botão de adicionar clicado para o produto ID:', productId);
+        // Adicione aqui a lógica para adicionar ao carrinho
       });
-    });
-
-    // Adiciona eventos de clique para as setas
-  arrowLeft.addEventListener('click', function () {
-    if (currentIndex > 0) {
-      currentIndex--;
-      showCards();
+    
+      content.appendChild(title);
+      content.appendChild(price);
+      content.appendChild(vendedor);
+      content.appendChild(addButton);
+    
+      card.appendChild(imgDiv);
+      card.appendChild(content);
+    
+      // Adiciona um ouvinte de evento de clique ao card
+      card.addEventListener('click', function () {
+        const productId = this.dataset.productId;
+        console.log('ID do produto clicado:', productId);
+      });
+    
+      return card;
     }
-  });
 
-  arrowRight.addEventListener('click', function () {
-    const cards = document.querySelectorAll('.card-product');
-    if (currentIndex < cards.length - 3) {
-      currentIndex++;
-      showCards();
-    }
-  });
+// Adiciona os cards à lista
+const cardList = document.querySelector('.product-list');
+products.forEach((product) => {
+  const card = createProductCard(product);
+  cardList.appendChild(card);
+});
+
+const mangaList = document.querySelector('.manga-list');
+products.forEach((product) => {
+  const card = createProductCard(product);
+  mangaList.appendChild(card);
+});
+
+const lightList = document.querySelector('.light-list');
+products.forEach((product) => {
+  const card = createProductCard(product);
+  lightList.appendChild(card);
+});
+
+const actList = document.querySelector('.act-list');
+products.forEach((product) => {
+  const card = createProductCard(product);
+  actList.appendChild(card);
+});
+
